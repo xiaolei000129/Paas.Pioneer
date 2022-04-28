88376eae06130c9d521163406179c3a01618d954
段丛磊 <1173514237@qq.com>
2022-04-28 23:36:03 +0800

    添加资讯模块新表

---
+++ modules/admin-core/src/Paas.Pioneer.Admin.Core.Domain/Grid/Information_GridEntity.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Paas.Pioneer.Domain;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace Paas.Pioneer.Admin.Core.Domain.Grid
+{
+    /// <summary>
+    /// 栅格管理
+    /// </summary>
+    [Table("Information_Grid")]
+    [Comment("栅格管理")]
+    public class Information_GridEntity : BaseEntity
+    {
+        /// <summary>
+        /// 字典Id
+        /// </summary>
+        [Comment("字典Id")]
+        [Column("DictionaryId", TypeName = "char(36)")]
+        public Guid DictionaryId { get; set; }
+
+        /// <summary>
+        /// 栅格管理类型
+        /// </summary>
+        [Comment("栅格管理类型")]
+        [Column("GridType", TypeName = "int")]
+        public EGridType GridType { get; set; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        [Comment("名称")]
+        [Column("Name", TypeName = "varchar(100)")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 图片
+        /// </summary>
+        [Comment("图片")]
+        [Column("Portrait", TypeName = "varchar(200)")]
+        public string Portrait { get; set; }
+
+        /// <summary>
+        /// 拓展信息（文章id、保存外部链接）
+        /// </summary>
+        [Comment("拓展信息")]
+        [Column("Expand", TypeName = "varchar(200)")]
+        public string Expand { get; set; }
+
+        /// <summary>
+        /// 排序
+        /// </summary>
+        [Comment("排序")]
+        [Column("Sort", TypeName = "int")]
+        [DefaultValue(0)]
+        public int Sort { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        [Comment("描述")]
+        [Column("Description", TypeName = "varchar(100)")]
+        public string Description { get; set; }
+    }
+
+    /// <summary>
+    /// 栅格管理类型
+    /// </summary>
+    public enum EGridType
+    {
+        /// <summary>
+        /// 未知
+        /// </summary>
+        [Description("未知")]
+        None = 0,
+
+        /// <summary>
+        /// 文章
+        /// </summary>
+        [Description("文章")]
+        Article = 1,
+
+        /// <summary>
+        /// 小程序
+        /// </summary>
+        [Description("小程序")]
+        MiniProgram = 2,
+
+        /// <summary>
+        /// 外链
+        /// </summary>
+        [Description("外链")]
+        ExternalLink = 3
+    }
+}
--- modules/admin-core/src/Paas.Pioneer.Admin.Core.EntityFrameworkCore/EntityFrameworkCore/AdminsDbContext.cs
+++ modules/admin-core/src/Paas.Pioneer.Admin.Core.EntityFrameworkCore/EntityFrameworkCore/AdminsDbContext.cs
@@ -8,9 +8,11 @@ using Paas.Pioneer.Admin.Core.Domain.Dictionary;
 using Paas.Pioneer.Admin.Core.Domain.DictionaryType;
 using Paas.Pioneer.Admin.Core.Domain.Document;
 using Paas.Pioneer.Admin.Core.Domain.DocumentImage;
+using Paas.Pioneer.Admin.Core.Domain.Grid;
 using Paas.Pioneer.Admin.Core.Domain.LoginLog;
 using Paas.Pioneer.Admin.Core.Domain.LowCodeTable;
 using Paas.Pioneer.Admin.Core.Domain.LowCodeTableConfig;
+using Paas.Pioneer.Admin.Core.Domain.News;
 using Paas.Pioneer.Admin.Core.Domain.Permission;
 using Paas.Pioneer.Admin.Core.Domain.PermissionApi;
 using Paas.Pioneer.Admin.Core.Domain.Personnel.Employee;
@@ -18,6 +20,7 @@ using Paas.Pioneer.Admin.Core.Domain.Personnel.Organization;
 using Paas.Pioneer.Admin.Core.Domain.Personnel.Position;
 using Paas.Pioneer.Admin.Core.Domain.Role;
 using Paas.Pioneer.Admin.Core.Domain.RolePermission;
+using Paas.Pioneer.Admin.Core.Domain.Slideshow;
 using Paas.Pioneer.Admin.Core.Domain.TenantPermission;
 using Paas.Pioneer.Admin.Core.Domain.User;
 using Paas.Pioneer.Admin.Core.Domain.UserRole;
@@ -63,7 +66,14 @@ namespace Paas.Pioneer.Admin.Core.EntityFrameworkCore.EntityFrameworkCore
         public DbSet<Pe_PositionEntity> Pe_PositionEntitys { get; set; }
         public DbSet<Ad_LowCodeTableConfigEntity> Ad_LowCodeTableConfigEntitys { get; set; }
         public DbSet<Ad_LowCodeTableEntity> Ad_LowCodeTableEntitys { get; set; }
+
+        
+        #region 资讯管理
         public DbSet<Information_CommentEntity> Information_CommentEntitys { get; set; }
+        public DbSet<Information_GridEntity> Information_GridEntitys { get; set; }
+        public DbSet<Information_NewsEntity> Information_NewsEntitys { get; set; }
+        public DbSet<Information_SlideshowEntity> Information_SlideshowEntitys { get; set; }
+        #endregion
 
         // Tenant Management
         public DbSet<Volo.Abp.TenantManagement.Tenant> Tenants { get; set; }
